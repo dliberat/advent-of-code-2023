ed9f029fb950fe7a5e5e2f42edc6b097fb3f9d12
Dan Liberatori <daniel.liberatori@gmail.com>
2024-11-17 10:13:27 +0900

    Solve Day 17 part 2

--- src/day17.rs
+++ src/day17.rs
@@ -5,6 +5,9 @@ use std::io::{ Lines, BufReader };
 use std::u32;
 use std::collections::BinaryHeap;
 
+const PART_1_STOPPING_DISTANCE: usize = 0;
+const PART_2_STOPPING_DISTANCE: usize = 4;
+
 #[derive(Debug, PartialEq)]
 struct Grid {
     grid: Vec<Vec<u32>>,
@@ -38,7 +41,8 @@ impl Grid {
 struct Location {
     row: usize,
     col: usize,
-    history: [char; 3]
+    dist: usize,
+    dir: char,
 }
 
 impl Location {
@@ -46,85 +50,53 @@ impl Location {
         Location {
             row: 0,
             col: 0,
-            history: ['.', '.', '.']
+            dir: '.',
+            dist: 0,
         }
     }
 
     fn down(&self) -> Self {
+        let dir = 'D';
+        let dist = if self.dir == dir { self.dist + 1 } else { 1 };
         Location{
             row: self.row+1,
             col: self.col,
-            history: [
-                self.history[1],
-                self.history[2],
-                'D',
-            ]
+            dir,
+            dist,
         }
     }
 
     fn up(&self) -> Self {
+        let dir = 'U';
+        let dist = if self.dir == dir { self.dist + 1 } else { 1 };
         Location{
             row: self.row-1,
             col: self.col,
-            history: [
-                self.history[1],
-                self.history[2],
-                'U',
-            ]
+            dir,
+            dist,
         }
     }
 
     fn left(&self) -> Self {
+        let dir = 'L';
+        let dist = if self.dir == dir { self.dist + 1 } else { 1 };
         Location{
             row: self.row,
             col: self.col-1,
-            history: [
-                self.history[1],
-                self.history[2],
-                'L',
-            ]
+            dir,
+            dist,
         }
     }
 
     fn right(&self) -> Self {
+        let dir = 'R';
+        let dist = if self.dir == dir { self.dist + 1 } else { 1 };
         Location{
             row: self.row,
             col: self.col+1,
-            history: [
-                self.history[1],
-                self.history[2],
-                'R',
-            ]
+            dir,
+            dist,
         }
-    }
-
-    fn can_move(&self, dir: char) -> bool {
-        let last = *self.history.last().unwrap();
-        if (dir == 'R' && last == 'L') ||
-            (dir == 'L' && last == 'R') ||
-            (dir == 'U' && last == 'D') ||
-            (dir == 'D' && last == 'U') {
-                return false;
-        }
-        self.history.iter().any(|c| *c != dir)
-    }
-
-    fn neighbors(&self, g: &Grid) -> Vec<Location> {
-        let mut neighbors = vec!();
-
-        if self.row < g.height -1 && self.can_move('D') {
-            neighbors.push(self.down());
-        }
-        if self.col < g.width - 1 && self.can_move('R') {
-            neighbors.push(self.right());
-        }
-        if self.col > 0 && self.can_move('L') {
-            neighbors.push(self.left());
-        }
-        if self.row > 0 && self.can_move('U') {
-            neighbors.push(self.up());
-        }
-        neighbors
     }
 }
 
@@ -146,19 +118,18 @@ impl PartialOrd for State {
     }
 }
 
-pub(crate) fn solve_part1(input: Lines<BufReader<File>>)  -> String {
+pub(crate) fn solve_part1(input: Lines<BufReader<File>>) -> String {
     let data: Vec<String> = input.map(|x| x.unwrap()).collect();
     let grid = parse_input(data);
-    let result = part_1_solver(&grid);
-    return result.to_string();
+    let result = dijkstra(&grid, part_1_neighbors_strategy, PART_1_STOPPING_DISTANCE);
+    result.to_string()
 }
 
 pub(crate) fn solve_part2(input: Lines<BufReader<File>>)  -> String {
-    // for line in input {
-    //     println!("{}", line.unwrap());
-    // }
-
-    return String::from("");
+    let data: Vec<String> = input.map(|x| x.unwrap()).collect();
+    let grid = parse_input(data);
+    let result = dijkstra(&grid, part_2_neighbors_strategy, PART_2_STOPPING_DISTANCE);
+    result.to_string()
 }
 
 fn parse_input(data: Vec<String>) -> Grid {
@@ -170,8 +141,69 @@ fn parse_input(data: Vec<String>) -> Grid {
     Grid{grid, height, width}
 }
 
-fn part_1_solver(grid: &Grid) -> u32 {
-    // Dijkstra
+fn part_1_neighbors_strategy(loc: &Location, g: &Grid) -> Vec<Location> {
+    let mut neighbors = vec!();
+
+    let can_move_down = (loc.dir != 'U') && !(loc.dir == 'D' && loc.dist == 3);
+    let can_move_up = (loc.dir != 'D') && !(loc.dir == 'U' && loc.dist == 3);
+    let can_move_left = (loc.dir != 'R') && !(loc.dir == 'L' && loc.dist == 3);
+    let can_move_right = (loc.dir != 'L') && !(loc.dir == 'R' && loc.dist == 3);
+
+    if loc.row < g.height -1 && can_move_down {
+        neighbors.push(loc.down());
+    }
+    if loc.col < g.width - 1 && can_move_right {
+        neighbors.push(loc.right());
+    }
+    if loc.col > 0 && can_move_left {
+        neighbors.push(loc.left());
+    }
+    if loc.row > 0 && can_move_up {
+        neighbors.push(loc.up());
+    }
+    neighbors
+}
+
+fn part_2_neighbors_strategy(loc: &Location, g: &Grid) -> Vec<Location> {
+    let mut neighbors = vec!();
+
+    // can probably optimize this further by checking that we can move the full 4 spaces.
+    // Then we'll never have a state where dist is < 4.
+    if loc.dist < 4 {
+        // special handling for the starting node
+        if loc.dir != '.' {
+            match loc.dir {
+                'D' => if loc.row < g.height - 1 { neighbors.push(loc.down()) },
+                'U' => if loc.row > 0 { neighbors.push(loc.up() )},
+                'R' => if loc.col < g.width - 1 { neighbors.push(loc.right() )},
+                'L' => if loc.col > 0 { neighbors.push(loc.left()) },
+                _ => panic!("illegal direction"),
+            }
+            return neighbors;
+        }
+    }
+
+    let can_move_down = (loc.dir != 'U') && !(loc.dir == 'D' && loc.dist == 10);
+    let can_move_up = (loc.dir != 'D') && !(loc.dir == 'U' && loc.dist == 10);
+    let can_move_left = (loc.dir != 'R') && !(loc.dir == 'L' && loc.dist == 10);
+    let can_move_right = (loc.dir != 'L') && !(loc.dir == 'R' && loc.dist == 10);
+
+    if loc.row < g.height -1 && can_move_down {
+        neighbors.push(loc.down());
+    }
+    if loc.col < g.width - 1 && can_move_right {
+        neighbors.push(loc.right());
+    }
+    if loc.col > 0 && can_move_left {
+        neighbors.push(loc.left());
+    }
+    if loc.row > 0 && can_move_up {
+        neighbors.push(loc.up());
+    }
+    neighbors
+}
+
+fn dijkstra(grid: &Grid, neighbors_strategy: fn(&Location, &Grid) -> Vec<Location>, stopping_distance: usize) -> u32 {
     let start_node = Location::new();
     let mut visited: HashMap<Location, u32> = HashMap::new();
     visited.insert(start_node.clone(), 0);
@@ -181,7 +213,7 @@ fn part_1_solver(grid: &Grid) -> u32 {
 
     while let Some(State{cost, loc: current}) = unvisited.pop() {
 
-        if grid.is_goal(&current) {
+        if grid.is_goal(&current) && current.dist >= stopping_distance {
             return cost;
         }
 
@@ -189,7 +221,7 @@ fn part_1_solver(grid: &Grid) -> u32 {
             continue;
         }
 
-        let neighbors = current.neighbors(&grid);
+        let neighbors = neighbors_strategy(&current, &grid);
         for neighbor in neighbors {
             
             let best_so_far = match visited.get(&neighbor) {
@@ -223,15 +255,15 @@ mod tests {
         let coord = Location::new();
         assert_eq!(1, grid.heat_loss(&coord));
 
-        let coord = Location{row: 0, col: 2, history: ['.', '.', '.']};
+        let coord = Location{row: 0, col: 2, dir: 'U', dist: 1};
         assert_eq!(3, grid.heat_loss(&coord));
 
-        let coord = Location{row: 1, col: 1, history: ['.', '.', '.']};
+        let coord = Location{row: 1, col: 1, dir: 'U', dist: 1};
         assert_eq!(5, grid.heat_loss(&coord));
     }
 
     #[test]
-    fn test_location_neighbors() {
+    fn test_part_1_neighbors_strat() {
         let grid = Grid::new(vec!(
             vec!(1, 9, 9, 1),
             vec!(1, 9, 9, 1),
@@ -239,7 +271,7 @@ mod tests {
             vec!(1, 1, 1, 1),
         ));
         let s = Location::new().down().right();
-        let neighbors = s.neighbors(&grid);
+        let neighbors = part_1_neighbors_strategy(&s, &grid);
         assert_eq!(3, neighbors.len());
         let mut down = false;
         let mut up = false;
@@ -259,7 +291,7 @@ mod tests {
     }
 
     #[test]
-    fn test_location_neighbors_with_move_limit() {
+    fn test_part_1_neighbors_with_move_limit() {
         let grid = Grid::new(vec!(
             vec!(1, 9, 9, 1, 1),
             vec!(1, 9, 9, 1, 1),
@@ -270,7 +302,7 @@ mod tests {
         assert_eq!(1, s.row);
         assert_eq!(3, s.col);
 
-        let neighbors = s.neighbors(&grid);
+        let neighbors = part_1_neighbors_strategy(&s, &grid);
         assert_eq!(2, neighbors.len());
         let mut down = false;
         let mut up = false;
@@ -293,7 +325,7 @@ mod tests {
             vec!(1, 1, 1),
         ));
 
-        let result = part_1_solver(&grid);
+        let result = dijkstra(&grid, part_1_neighbors_strategy, PART_1_STOPPING_DISTANCE);
 
         assert_eq!(4, result);
     }
@@ -310,7 +342,7 @@ mod tests {
         ));
 
         
-        let result = part_1_solver(&grid);
+        let result = dijkstra(&grid, part_1_neighbors_strategy, PART_1_STOPPING_DISTANCE);
         assert_eq!(13, result);
     }
 
@@ -331,7 +363,42 @@ mod tests {
             vec!(2,5,4,6,5,4,8,8,8,7,7,3,5),
             vec!(4,3,2,2,6,7,4,6,5,5,5,3,3),
         ));
-        let result = part_1_solver(&grid);
+        let result = dijkstra(&grid, part_1_neighbors_strategy, PART_1_STOPPING_DISTANCE);
         assert_eq!(102, result);
+    }
+
+
+    #[test]
+    fn test_part_2_solver_1() {
+        let grid = Grid::new(vec!(
+            vec!(2,4,1,3,4,3,2,3,1,1,3,2,3),
+            vec!(3,2,1,5,4,5,3,5,3,5,6,2,3),
+            vec!(3,2,5,5,2,4,5,6,5,4,2,5,4),
+            vec!(3,4,4,6,5,8,5,8,4,5,4,5,2),
+            vec!(4,5,4,6,6,5,7,8,6,7,5,3,6),
+            vec!(1,4,3,8,5,9,8,7,9,8,4,5,4),
+            vec!(4,4,5,7,8,7,6,9,8,7,7,6,6),
+            vec!(3,6,3,7,8,7,7,9,7,9,6,5,3),
+            vec!(4,6,5,4,9,6,7,9,8,6,8,8,7),
+            vec!(4,5,6,4,6,7,9,9,8,6,4,5,3),
+            vec!(1,2,2,4,6,8,6,8,6,5,5,6,3),
+            vec!(2,5,4,6,5,4,8,8,8,7,7,3,5),
+            vec!(4,3,2,2,6,7,4,6,5,5,5,3,3),
+        ));
+        let result = dijkstra(&grid, part_2_neighbors_strategy, PART_2_STOPPING_DISTANCE);
+        assert_eq!(94, result);
+    }
+
+    // #[test]
+    fn test_part_2_solver_2() {
+        let grid = Grid::new(vec!(
+            vec!(1,1,1,1,1,1,1,1,1,1,1,1),
+            vec!(9,9,9,9,9,9,9,9,9,9,9,1),
+            vec!(9,9,9,9,9,9,9,9,9,9,9,1),
+            vec!(9,9,9,9,9,9,9,9,9,9,9,1),
+            vec!(9,9,9,9,9,9,9,9,9,9,9,1),            
+        ));
+        let result = dijkstra(&grid, part_2_neighbors_strategy, PART_2_STOPPING_DISTANCE);
+        assert_eq!(71, result);
     }
 }
